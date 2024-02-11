0197db83433c34b81de31d60663b6f77c45eed11
MargaritaRoda <MargaritaRoda1984@gmail.com>
2024-02-11 19:29:11 +0300

    feat: implement simple-tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,72 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const input = {
+      a: NaN,
+      b: 3,
+      action: Action.Add,
+    };
+    const res = simpleCalculator(input);
+    expect(res).toBeNaN();
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const input = {
+      a: NaN,
+      b: 2,
+      action: Action.Subtract,
+    };
+    const res = simpleCalculator(input);
+    expect(res).toBeNaN();
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const input = {
+      a: NaN,
+      b: 2,
+      action: Action.Multiply,
+    };
+    const res = simpleCalculator(input);
+    expect(res).toBeNaN();
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const input = {
+      a: NaN,
+      b: 2,
+      action: Action.Divide,
+    };
+    const res = simpleCalculator(input);
+    expect(res).toBeNaN();
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const input = {
+      a: NaN,
+      b: 2,
+      action: Action.Exponentiate,
+    };
+    const res = simpleCalculator(input);
+    expect(res).toBeNaN();
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const invalidInput = {
+      a: 1,
+      b: 2,
+      action: 'InvalidAction' as Action,
+    };
+    expect(() => simpleCalculator(invalidInput)).not.toThrow();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const invalidInput = {
+      a: 'invalid',
+      b: 2,
+      action: Action.Add,
+    };
+    expect(simpleCalculator(invalidInput)).toBeNull();
   });
 });
